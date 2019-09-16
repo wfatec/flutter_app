8a5e31505f1c0b450d20e883ef64a18dad91f3a5
wfatec <chao125010@163.com>
2019-09-16 14:59:00 +0800

    route management

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,12 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.purple,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      //注册路由表
+      routes:{
+        "new_page":(context)=>NewRoute(),
+        "/":(context)=> MyHomePage(title: 'Flutter Demo Home Page'), //注册首页路由
+      } ,
+//      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
@@ -98,6 +103,33 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.display1,
             ),
+            FlatButton(
+              child: Text("open new route"),
+              textColor: Colors.blue,
+              onPressed: () {
+                //导航到新路由
+                Navigator.pushNamed(context, "new_page");
+              },
+            ),
+            RaisedButton(
+              onPressed: () async {
+                // 打开`TipRoute`，并等待返回结果
+                var result = await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return TipRoute(
+                        // 路由参数
+                        text: "我是提示xxxx",
+                      );
+                    },
+                  ),
+                );
+                //输出`TipRoute`路由返回结果
+                print("路由返回值: $result");
+              },
+              child: Text("打开提示页"),
+            ),
           ],
         ),
       ),
@@ -106,6 +138,51 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class NewRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("New route"),
+      ),
+      body: Center(
+        child: Text("This is new route"),
+      ),
+    );
+  }
+}
+
+class TipRoute extends StatelessWidget {
+  TipRoute({
+    Key key,
+    @required this.text,  // 接收一个text参数
+  }) : super(key: key);
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("提示"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(18),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text(text),
+              RaisedButton(
+                onPressed: () => Navigator.pop(context, "我是返回值"),
+                child: Text("返回"),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
